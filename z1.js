4050524d3db1cdc7db3baf5a5e1d05aa58cae9fd
Adam29C <adamcatholic29@gmail.com>
2024-10-10 17:47:43 +0530

    remove file

--- z1.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-const currentDate = moment('08/10/2024', 'DD/MM/YYYY'); // Specify the input format
-const daysToAdd = 1; // Number of days to add
-
-const futureDate = currentDate.add(daysToAdd, 'days');
-
-console.log(futureDate.format('DD/MM/YYYY')); // Outputs the new date in 'DD/MM/YYYY' format
